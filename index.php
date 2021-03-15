1af47a732a8a14c0649885cc2178c27eb37322f9
yuzucha01 <78517616+yuzucha01@users.noreply.github.com>
2021-03-16 00:13:25 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,16 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="css/style.css" />
   </head>
   <body>
-    <form method="post" 　action="insert.php">
+
+    <form method="post" action="insert.php">
       <p>名前を入力してください</p>
       <input name="name" type="text" placeholder="名前を入力してください" />
       <p>メールアドレスを入力してください</p>
       <input name="email" type="text" placeholder="メールを入力" />
       <p>パスワードを入力してください</p>
-      <input name="password" type="text" placeholder="パスワードを入力" />
+      <input type="text" name="password" id="">
       <input type="submit" value="登録する" />
     </form>
   </body>
